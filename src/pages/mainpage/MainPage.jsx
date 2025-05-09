708f4368e079fb9281d8064ee0ee94dcb383bd6d
DavinLee <davin11992@gmail.com>
2025-05-09 12:08:08 +0900

    [fix] Header import문 /header에서 /Header로

--- src/pages/mainpage/MainPage.jsx
+++ src/pages/mainpage/MainPage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "./MainPage.css";
 import KakaoMap from "../../components/mainpage/KaKaoMap";
-import Header from "../../components/common/header";
+import Header from "../../components/common/Header";
 
 const MainPage = () => {
   return (
     <div className="mainpage">
       <Header />
       <KakaoMap />
-      
     </div>
   );
 };
