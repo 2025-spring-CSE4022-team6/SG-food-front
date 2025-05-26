61dfe42558e32d84d36b64f6a23690cab9878e3a
DavinLee <davin11992@gmail.com>
2025-05-26 17:42:38 +0900

    [fix] 탭 제목 PubPick으로 수정

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ import RestaurantDetail from "./pages/restaurant/RestaurantDetail";
 import MyPage from "./components/mypage/MyPage";
 import ReviewWrite from "./pages/ReviewWrite";
 
-localStorage.setItem("accessToken", "test-token"); // 강제로 로그인된 상태 만들기
-//localStorage.removeItem("accessToken"); // 로그아웃 상태로 만들기
+//localStorage.setItem("accessToken", "test-token"); // 강제로 로그인된 상태 만들기
+localStorage.removeItem("accessToken"); // 로그아웃 상태로 만들기
 
 function App() {
   return (
